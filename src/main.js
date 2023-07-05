7d706f12871992194fbd85f0bb1750530c53c104
Dmitry Zyryanov <r8cdf@yandex.ru>
2023-07-05 23:19:03 +0500

    Добавил переменную для fake api, пути для auth

--- src/main.js
+++ src/main.js
@@ -4,6 +4,10 @@ import vuetify from './plugins/vuetify'
 import router from './_helpers/router';
 import store from './store/index';
 
+// setup fake backend
+import { configureFakeBackend } from './_helpers';
+configureFakeBackend();
+
 Vue.config.productionTip = false
 
 new Vue({
