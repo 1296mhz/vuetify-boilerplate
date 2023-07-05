ee27e47e0f5b4a7a3f6d38acc594fcf7a36bd30c
Dmitry Zyryanov <r8cdf@yandex.ru>
2023-07-05 23:44:54 +0500

    НЕ кастуется env переменная

--- src/main.js
+++ src/main.js
@@ -3,10 +3,15 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import router from './_helpers/router';
 import store from './store/index';
-
-// setup fake backend
 import { configureFakeBackend } from './_helpers';
-configureFakeBackend();
+const vueAppFakeApi = new Boolean(process.env.VUE_APP_FAKE_API);
+console.log("vueAppFakeApi")
+console.log(vueAppFakeApi)
+// setup fake backend
+if (vueAppFakeApi) {
+  configureFakeBackend();
+}
+
 
 Vue.config.productionTip = false
 
